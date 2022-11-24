b600608eb41210a94d97ff2cac9ffdb7b8cb96bf
quardianwolf <quardianwolf@gmail.com>
2022-11-24 22:53:55 +0300

    bug fix7

--- components/BeforeAfter.js
+++ components/BeforeAfter.js
@@ -24,7 +24,7 @@ const BeforeAfter = () => {
                 <ReactBeforeSliderComponent
                 firstImage={FIRST_IMAGE}
                 secondImage={SECOND_IMAGE}
-                />
+                 alt="img" />
             </div>
         </div>
     </div>
